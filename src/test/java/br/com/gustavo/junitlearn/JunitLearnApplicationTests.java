1b3f1487100ca59e7907a37a6786898ab3ea3172
Gustavo Linhaus Fernandes <gustavo.fernandes@basis.com.br>
2022-08-03 11:57:13 -0300

    Start project

---
+++ src/test/java/br/com/gustavo/junitlearn/JunitLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.junitlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
